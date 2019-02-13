a8661719e2a7b07f381ccd13ffb1975317bd10a7
Макаров Денис <limitofzero@gmail.com>
2019-02-13 22:49:11 +0300

    mousemove listening

--- projects/ngx-graphs-diagramms/src/lib/components/diagramm-widget/diagramm-widget.component.ts
+++ projects/ngx-graphs-diagramms/src/lib/components/diagramm-widget/diagramm-widget.component.ts
@@ -1,6 +1,17 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
 import { NodeModel } from '../../models/node.model';
 import { BaseModel } from '../../models/base.model';
+import { Subject } from 'rxjs/internal/Subject';
+import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import { filter, takeUntil, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-diagramm-widget',
@@ -8,15 +19,43 @@ import { BaseModel } from '../../models/base.model';
   styleUrls: ['./diagramm-widget.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DiagrammWidgetComponent {
+export class DiagrammWidgetComponent implements AfterViewInit, OnDestroy {
+  private readonly onDestroy = new Subject<void>();
+
   private selectedEntityId: number = null;
+  private clickedEntityId: number = null;
 
   @Input()
   nodes: NodeModel[] = [];
 
+  @ViewChild('diagramWidget') diagramWidget: ElementRef;
+
   onEntityMouseDown(entity: BaseModel): void {
     if (entity) {
       this.selectedEntityId = entity.id;
+      this.clickedEntityId = entity.id;
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.initListeningMouseMoveEvent();
+  }
+
+  private initListeningMouseMoveEvent(): void {
+    fromEvent(this.diagramWidget.nativeElement, 'mousemove').pipe(
+      takeUntil(this.onDestroy),
+      throttleTime(100),
+      filter(() => !!this.clickedEntityId)
+    ).subscribe({
+      next: event => this.onMouseMoveHandler(event)
+    });
+  }
+
+  private onMouseMoveHandler(event: any): void {
+    console.log(event);
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy.complete();
   }
 }
