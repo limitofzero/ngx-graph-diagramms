b1617257d9cc3d8c8416d351a45d6a6921605d8f
Макаров Денис <limitofzero@gmail.com>
2019-02-27 09:35:25 +0300

    Удаление width & height. Эти данные необходимо будет считать в рантайме

--- projects/ngx-graphs-diagramms/src/lib/models/node.model.ts
+++ projects/ngx-graphs-diagramms/src/lib/models/node.model.ts
@@ -6,8 +6,6 @@ export class NodeModel extends BaseModel {
   x = 0;
   y = 0;
   readonly type: string;
-  width: number;
-  height: number;
 
   ports: ModelMap<PortModel> = {};
 
@@ -16,15 +14,11 @@ export class NodeModel extends BaseModel {
     id?: string,
     x?: number,
     y?: number,
-    width?: number,
-    height?: number,
     ports?: ModelMap<PortModel>
   }) {
     super(data.id);
-    const { type, x, y, width, height, ports } = data;
+    const { type, x, y, ports } = data;
     this.type = type;
-    this.width = width || 0;
-    this.height = height || 0;
     this.x = x || 0;
     this.y = y || 0;
     this.ports = ports || {};
@@ -40,12 +34,6 @@ export class NodeModel extends BaseModel {
     cloned.y = y;
 
     return cloned;
-  }
-
-  setDimensionsAndClone(width: number, height: number): NodeModel {
-    this.height = height;
-    this.width = width;
-    return this.clone();
   }
 }
 
